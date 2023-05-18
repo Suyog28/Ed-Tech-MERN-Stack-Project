f1c62c0af36fb1ad77bdb2a9f0d299149d79afbd
Suyog28 <suyogvmuley@gmail.com>
2023-05-18 09:05:13 +0530

    Section creation

---
+++ controllers/Section.js
@@ -0,0 +1,10 @@
+const Section = require("../models/Section");
+const Course = require("../models/Course");
+
+exports.createSection = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
